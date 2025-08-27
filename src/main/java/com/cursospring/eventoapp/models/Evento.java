4207777ca47b3078d5f2af91ca6494b1b215928c
tiagomatos86 <tiago_m_lima@hotmail.com>
2025-08-27 03:09:23 -0300

    Configurando application,properties para conectar com o BD, Transformando a classe Evento em uma entidade

--- src/main/java/com/cursospring/eventoapp/models/Evento.java
+++ src/main/java/com/cursospring/eventoapp/models/Evento.java
@@ -1,11 +1,47 @@
 package com.cursospring.eventoapp.models;
 
+// import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="eventos")
 public class Evento {
+// public class Evento implements Serializable {
+
+    // private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    
     private String nome;
     private String local;
     private String data;
     private String horario;
 
+    public Evento() {}
+
+    public Evento(long id, String nome, String local, String data, String horario) {
+        this.id = id;
+        this.nome = nome;
+        this.local = local;
+        this.data = data;
+        this.horario = horario;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+    
     public String getNome() {
         return nome;
     }
@@ -29,5 +65,11 @@ public class Evento {
     }
     public void setHorario(String horario) {
         this.horario = horario;
+    }
+
+    @Override
+    public String toString() {
+        return "Evento [id=" + id + ", nome=" + nome + ", local=" + local + ", data=" + data + ", horario=" + horario
+                + "]";
     }
 }
