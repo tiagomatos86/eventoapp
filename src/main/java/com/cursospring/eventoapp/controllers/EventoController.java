8fde814fb6d1bdf84e02af28cb42c1ed03da3983
tiagomatos86 <tiago_m_lima@hotmail.com>
2025-08-29 04:36:32 -0300

    Configurando a view para carregar os eventos cadastrados no BD

--- src/main/java/com/cursospring/eventoapp/controllers/EventoController.java
+++ src/main/java/com/cursospring/eventoapp/controllers/EventoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.cursospring.eventoapp.models.Evento;
 import com.cursospring.eventoapp.repository.EventoRepository;
@@ -23,6 +24,14 @@ public class EventoController {
     public String form(Evento evento) {
         er.save(evento);
         return "redirect:/cadastro-eventos";
+    }
+
+    @RequestMapping(value = "/eventos")
+    public ModelAndView listaEventos(){
+        ModelAndView mv = new ModelAndView("index");
+        Iterable<Evento> eventos = er.findAll();
+        mv.addObject("eventos", eventos);
+        return mv;
     }
     
 }
